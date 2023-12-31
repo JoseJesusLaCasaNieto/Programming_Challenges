f208e34fca51c23735e8e23532b11b20284142fb
José Jesús La Casa Nieto <148141178+JoseJesusLaCasaNieto@users.noreply.github.com>
2023-12-31 12:56:58 +0100

    Merge Reto 7 2022 a main

---
+++ 2022/Invirtiendo_Cadenas.py
@@ -0,0 +1,11 @@
+# /*
+#  * Crea un programa que invierta el orden de una cadena de texto
+#  * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+#  * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+#  */
+
+entrance_string = input("Introduzca una cadena de texto para invertir: ")
+
+print("\nCadena de texto invertida: {}".format(entrance_string[::-1]))
+
+
